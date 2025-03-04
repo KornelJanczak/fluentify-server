454a9091d18e38507dc6c6dd913e076eb1506027
KornelJanczak <korneljanczak10@gmail.com>
2025-03-04 23:24:52 +0100

    feat: integrate ConfigModule, RedisModule, and AuthModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './common/config/configuration';
+import { RedisModule } from './shared/redis/redis.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    RedisModule,
+    AuthModule,
+  ],
   providers: [AppService],
+  controllers: [],
 })
 export class AppModule {}
