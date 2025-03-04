9a0dd040c159fc58e70e598efd68c83e1d33ef04
KornelJanczak <korneljanczak10@gmail.com>
2025-03-04 23:25:15 +0100

    feat: add HttpExceptionFilter for centralized HTTP exception handling

---
+++ src/common/filters/http-exception-filter.ts
@@ -0,0 +1,30 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Logger } from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+  public catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    const errResponse = {
+      statusCode: status,
+      name: exception.name,
+      message: exception.message,
+      stack: exception.stack,
+      path: request.url,
+    };
+
+    this.logger.error(errResponse);
+    response.status(status).json(errResponse);
+  }
+}
