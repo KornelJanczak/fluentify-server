93391ad138019a7c649c54bdf6e7661ebb706bb4
KornelJanczak <korneljanczak10@gmail.com>
2025-03-05 20:00:35 +0100

    feat: add SessionSerializerService for user session management in authentication

---
+++ src/modules/auth/session-serializer.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { PassportSerializer } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { type User } from 'src/shared/db/schema';
+import { type VerifyCallback } from 'passport-google-oauth20';
+
+@Injectable()
+export class SessionSerializerService extends PassportSerializer {
+  private readonly logger = new Logger(SessionSerializerService.name);
+
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
+
+  public serializeUser({ id }: User, done: VerifyCallback) {
+    this.logger.log(`User has been serialized: ${id}`);
+    done(null, id);
+  }
+
+  public async deserializeUser(id: string, done: VerifyCallback) {
+    const currentUser = await this.authService.getUserById(id);
+    this.logger.log(`User has been deserialized: ${currentUser.email}`);
+    return currentUser ? done(null, currentUser) : done(null, null);
+  }
+}
