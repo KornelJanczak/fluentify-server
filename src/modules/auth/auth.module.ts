8a5180cae3a781ae5e3d4a58950ee7b70bc1d259
KornelJanczak <korneljanczak10@gmail.com>
2025-03-05 20:00:41 +0100

    feat: update AuthModule to include GoogleStrategy, SessionSerializerService, and UserRepository

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { GoogleStrategy } from './strategies/google.strategy';
+import UserRepository from 'src/shared/repositories/user.repository';
+import { DbModule } from 'src/shared/db/db.module';
+import { SessionSerializerService } from './session-serializer.service';
 
 @Module({
+  imports: [DbModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    SessionSerializerService,
+    GoogleStrategy,
+    UserRepository,
+  ],
 })
 export class AuthModule {}
