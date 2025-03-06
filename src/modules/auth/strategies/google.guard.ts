f7b987f9c65d86ec76e665b5182450f9d472e832
KornelJanczak <korneljanczak10@gmail.com>
2025-03-06 23:57:27 +0100

    feat: enhance GoogleAuthGuard with logging for better context debugging

--- src/modules/auth/strategies/google.guard.ts
+++ src/modules/auth/strategies/google.guard.ts
@@ -4,9 +4,15 @@ import { Request } from 'express';
 
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
-  async canActivate(context: ExecutionContext) {
+  public async canActivate(context: ExecutionContext) {
+    console.log('sadasd', context);
+
     const activate = (await super.canActivate(context)) as boolean;
+    console.log('activate', activate);
+
     const request = context.switchToHttp().getRequest<Request>();
+    console.log('request', request);
+
     await super.logIn(request);
     return activate;
   }
