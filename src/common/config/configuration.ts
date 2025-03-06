140028f8b01e94cd972ef21434ae82530068d6a6
KornelJanczak <korneljanczak10@gmail.com>
2025-03-06 23:57:06 +0100

    feat: add GOOGLE_APPLICATION_CREDENTIALS to configuration

--- src/common/config/configuration.ts
+++ src/common/config/configuration.ts
@@ -18,6 +18,7 @@ export default () => ({
     clientId: process.env.GOOGLE_CLIENT_ID || '',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     callbackUrl: process.env.GOOGLE_CALLBACK_URL || '',
+    applicationCredentials: process.env.GOOGLE_APPLICATION_CREDENTIALS || '',
   },
   aws: {
     bucketName: process.env.AWS_BUCKET_NAME || '',
