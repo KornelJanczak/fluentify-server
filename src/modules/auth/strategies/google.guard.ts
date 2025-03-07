4769444e94b9a9e7543ab0c1004a0086f32c1884
KornelJanczak <korneljanczak10@gmail.com>
2025-03-07 22:14:07 +0100

    refactor: remove unnecessary console logs from GoogleAuthGuard

--- src/modules/auth/strategies/google.guard.ts
+++ src/modules/auth/strategies/google.guard.ts
@@ -5,14 +5,10 @@ import { Request } from 'express';
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
   public async canActivate(context: ExecutionContext) {
-    console.log('sadasd', context);
-
     const activate = (await super.canActivate(context)) as boolean;
     console.log('activate', activate);
-
     const request = context.switchToHttp().getRequest<Request>();
     console.log('request', request);
-
     await super.logIn(request);
     return activate;
   }
