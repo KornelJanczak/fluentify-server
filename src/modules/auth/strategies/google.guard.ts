e2525465e2965a33ffd072740127c0ec591b7dca
KornelJanczak <korneljanczak10@gmail.com>
2025-03-10 23:33:16 +0100

    refactor: enhance GoogleAuthGuard to support offline access type and improve authentication flow

--- src/modules/auth/strategies/google.guard.ts
+++ src/modules/auth/strategies/google.guard.ts
@@ -4,12 +4,20 @@ import { Request } from 'express';
 
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
+  constructor() {
+    super({
+      accessType: 'offline',
+    });
+  }
+
   public async canActivate(context: ExecutionContext) {
-    const activate = (await super.canActivate(context)) as boolean;
-    console.log('activate', activate);
     const request = context.switchToHttp().getRequest<Request>();
-    console.log('request', request);
+
+    if (request.isAuthenticated()) return true;
+
+    const activate = (await super.canActivate(context)) as boolean;
     await super.logIn(request);
+
     return activate;
   }
 }
