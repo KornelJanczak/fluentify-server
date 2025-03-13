30da2e2e167b8cc400cd5bdd3c860edbc7bfa340
KornelJanczak <korneljanczak10@gmail.com>
2025-03-13 23:50:12 +0100

    refactor: update ServiceErrorFilter to handle ServiceError exceptions and improve error response structure

--- src/common/filters/service-error-filter.ts
+++ src/common/filters/service-error-filter.ts
@@ -3,30 +3,33 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { Logger } from '@nestjs/common';
+import { ServiceError } from '../service-error';
 
-@Catch(Error)
-export class ServiceE implements ExceptionFilter {
-  private readonly logger = new Logger(ServiceE.name);
+@Catch(ServiceError)
+export class ServiceErrorFilter implements ExceptionFilter {
+  private readonly logger = new Logger(ServiceErrorFilter.name);
   public catch(exception: HttpException, host: ArgumentsHost) {
-    this.logger.error(exception);
+    this.logger.error(
+      `${exception.name}: ${exception.message}`,
+      exception.stack,
+    );
 
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
 
     const errResponse = {
-      statusCode: status,
-      name: exception.name,
-      message: exception.message,
-      stack: exception.stack,
-      path: request.url,
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      name: 'Internal Server Error',
+      message: 'Server Error',
     };
 
-    this.logger.error(errResponse);
-    response.status(status).json(errResponse);
+    if (exception instanceof ServiceError.NotFoundError)
+      errResponse.statusCode = HttpStatus.NOT_FOUND;
+
+    response.status(errResponse.statusCode).json(errResponse);
   }
 }
