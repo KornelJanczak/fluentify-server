6d8cbf4b8c578f07e77830df6379c5ecc4304bd7
KornelJanczak <korneljanczak10@gmail.com>
2025-03-13 23:50:02 +0100

    refactor: reorder module imports in app.module.ts for improved organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import configuration from './common/config/configuration';
+import { DbModule } from './shared/db/db.module';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 import { RedisModule } from './shared/redis/redis.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { DbModule } from './shared/db/db.module';
-import { PassportModule } from '@nestjs/passport';
 import { VocabularySetModule } from './modules/vocabulary-set/vocabulary-set.module';
 
 @Module({
