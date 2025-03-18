3d124b41554a2a9d8faf20c4bf45773fc6af7082
KornelJanczak <korneljanczak10@gmail.com>
2025-03-18 23:57:03 +0100

    feat: add new interfaces for audio streaming and system prompt handling

--- src/modules/chat/chat.interfaces.ts
+++ src/modules/chat/chat.interfaces.ts
@@ -1,4 +1,6 @@
 import * as googleCloud from '@google-cloud/text-to-speech';
+import type { CoreMessage, DataStreamWriter } from 'ai';
+import { Response } from 'express';
 
 export interface IGenerateAudioRequest
   extends googleCloud.protos.google.cloud.texttospeech.v1
@@ -10,3 +12,27 @@ export interface IAudioContent
 
 export interface IVoice
   extends googleCloud.protos.google.cloud.texttospeech.v1.IVoice {}
+
+export interface StartStreamRequest {
+  chatId: string;
+  tutorId: string;
+  studyingLanguageLevel: string;
+  messages: CoreMessage[];
+  res: Response;
+}
+
+export interface OnFinishStreamArgs {
+  chatId: string;
+  tutorId: string;
+  content: string;
+  usedTokens: number;
+  streamWriter: DataStreamWriter;
+}
+
+export interface GetSystemPromptArgs {
+  tutorId: string;
+  studyingLanguageLevel: string;
+  chatCategory: string;
+  chatTopic: string;
+  vocabularySetId?: string;
+}
