7d3ce12ed930630bd983515abe3967824b8a8566
KornelJanczak <korneljanczak10@gmail.com>
2025-03-18 23:57:06 +0100

    feat: enhance ChatModule by adding new services and repositories

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ChatStreamService } from './services/chat-stream.service';
+import { AudioGeneratorService } from './services/audio-generator.service';
+import { SystemPromptService } from './services/system-prompt.service';
+import { TutorPromptService } from './services/tutor-prompt.service';
+import { ChatRepository } from 'src/shared/repositories/chat.repository';
+import { DbModule } from 'src/shared/db/db.module';
+import { AiModule } from 'src/shared/ai/ai.module';
+import { TopicPromptFactoryService } from './services/topic-prompt-factory.service';
+import VocabularySetRepository from 'src/shared/repositories/vocabulary-set.repository';
 
 @Module({
-  providers: [ChatService],
-  controllers: [ChatController]
+  imports: [DbModule, AiModule],
+  providers: [
+    ChatRepository,
+    VocabularySetRepository,
+    ChatService,
+    ChatStreamService,
+    AudioGeneratorService,
+    SystemPromptService,
+    TutorPromptService,
+    TopicPromptFactoryService,
+  ],
+  controllers: [ChatController],
 })
 export class ChatModule {}
